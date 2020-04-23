a1dcf272a59e82f2c4baefc1f4bdb1c31e3dca8d
Do Phu An <an.do@production.com.vn>
2020-04-23 15:39:22 +0700

    1PAC feat: Add Top Headlines and Navbar

---
+++ components/TopHeadlines/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="headlines-container"></div>
+</template>
+
+<script>
+  export default {
+    name: 'TopHeadlines',
+    data: () => {
+      return {
+        newsList: []
+      }
+    },
+    mounted() {
+      this.fetchData()
+    },
+    methods: {
+      fetchData() {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .headlines-container {
+
+  }
+</style>
---
+++ model/News.js
@@ -0,0 +1,7 @@
+import Model from '../Model'
+
+export default class News extends Model {
+  resource() {
+    return 'news'
+  }
+}
